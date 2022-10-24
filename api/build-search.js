360809a8f8e6875fd710b6e83109686b904f2785
fstrauf <f.strauf@gmail.com>
2022-10-24 13:51:34 +1100

    backend cleanup

--- api/build-search.js
+++ api/build-search.js
@@ -10,7 +10,7 @@ export const algoliaInstance = algoliasearch(
 )
 
 
-export default async function handler(req, res) {
+export default async function handler(request, response) {
   const documents = await getPostsForSearch(false)
   const index = algoliaInstance.initIndex(process.env['ALGOLIA_INDEX'])
 
@@ -19,15 +19,17 @@ export default async function handler(req, res) {
     console.time(`Saving ${documents.length} documents to index:`)
     await index.saveObjects(documents)
     console.timeEnd(`Saving ${documents.length} documents to index:`)
-    return {
-      statusCode: 200,
-      body: 'Success!',
-    }
+    response.status(200).json({
+      body: 'Success',
+      query: request.query,
+      cookies: request.cookies,
+    });
   } catch (error) {
     console.error(error)
-    return {
-      statusCode: 500,
+    response.status(500).json({
       body: error,
-    }
+      query: request.query,
+      cookies: request.cookies,
+    });
   }
 }
