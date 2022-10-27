8a01188ed6cda5456e307f165d3077b8c0194578
Ranzelpanz <viktoria.strauf@gmail.com>
2022-10-28 08:45:27 +1100

    colors

--- components/breakdown-box.js
+++ components/breakdown-box.js
@@ -2,7 +2,7 @@ export default function BreakdownBox({ value, title }) {
   return (
     <div className="m-2">
       <label className="font-bold">{title}</label>
-      <div className="dark:text-white">{value}</div>
+      <div className="">{value}</div>
     </div>
   )
 }
--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head />
-        <body className='dark:bg-dark-tdao'>
+        <body className='bg-white'>
           <Main />
           <NextScript />
         </body>
