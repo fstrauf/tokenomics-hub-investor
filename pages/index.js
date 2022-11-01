daefec9f8e03ea9a4e7aa17fb6041002eb09727d
fstrauf <f.strauf@gmail.com>
2022-11-02 09:48:30 +1100

    changes to component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Container from '../components/container'
 import MoreStories from '../components/more-stories'
-// import HeroPost from '../components/hero-post'
 import Intro from '../components/intro'
 import Layout from '../components/layout'
 import { getAllPostsForHome } from '../lib/api'
@@ -19,16 +18,6 @@ export default function Index({ allPosts, preview }) {
         </Head>
         <Container>
           <Intro />
-          {/* {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              // date={heroPost.date}
-              // author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )} */}
           {morePosts.length > 0 && <MoreStories posts={morePosts} />}
         </Container>
       </Layout>
