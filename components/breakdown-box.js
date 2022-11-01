c87254ecbd8e90828c464bf82c9429dfbaccb750
fstrauf <f.strauf@gmail.com>
2022-11-02 08:56:26 +1100

    custom control for circular progress

--- components/breakdown-box.js
+++ components/breakdown-box.js
@@ -1,94 +1,23 @@
+import { CircularProgressbar } from 'react-circular-progressbar';
+
 export default function BreakdownBox({ value, strength, title }) {
 
-  const percentageComplete = strength / 10
-  const strokeDashOffsetValue = 100 - (percentageComplete * 100)
+  const percentageComplete = strength * 10
 
   return (
     <div className="m-2">
       <div class="flex">
         {/* <div className="grid grid-cols-1"> */}
         <div className="flex flex-col">
-        <h1 className="ml-2 mr-2 font-bold text-center">{title}</h1>
-          {/* <div class="flex"> */}
-          <div class="justify-center m-2 inline-flex items-center overflow-hidden rounded-full bottom-5 left-5">
-            <svg class="w-10 h-10" viewBox="-3 -3 38 38">
-              {/* Background */}
-              <circle
-                class="text-gray-300"
-                stroke-width="5"
-                stroke="currentColor"
-                fill="transparent"
-                r="15.9155"
-                cx="16"
-                cy="16"
-              />
-              {/* Percentage value */}
-              <circle
-                class="text-blue-600"
-                stroke-width="5"
-                stroke-dasharray="100 100"
-                stroke-dashoffset={strokeDashOffsetValue}
-                stroke-linecap="round"
-                stroke="currentColor"
-                fill="transparent"
-                transition="stroke-dashoffset 1s ease-in-out"
-                r="15.9155"
-                cx="16"
-                cy="16"
-              />
-            </svg>
-            <span class="absolute text-xl text-blue-700">{strength}</span>
+          <h1 className="ml-2 mr-2 font-bold text-center">{title}</h1>
+          <div className='w-10 h-10'>
+            <CircularProgressbar value={percentageComplete} text={`${percentageComplete}`} />
           </div>
-          
+          {/* <div class="flex"> */}
+          {/* <div class="justify-center m-2 inline-flex items-center overflow-hidden rounded-full bottom-5 left-5"> */}
         </div>
         <div className="w-full self-center">{value}</div>
       </div>
     </div>
   )
 }
-
-
-// export default function BreakdownBox({ value, strength, title }) {
-
-//   const percentageComplete = strength / 10
-//   const strokeDashOffsetValue = 100 - (percentageComplete * 100)
-
-//   return (
-//     <div className="m-2">
-//       {/* <div class="grid grid-rows-2 grid-flow-col auto-cols-max">  */}
-//       <div class="flex">
-//         <div class="justify-center m-2 inline-flex items-center overflow-hidden rounded-full bottom-5 left-5">
-//           <svg class="w-10 h-10" viewBox="-1 -1 34 34">
-//             {/* Background */}
-//             <circle
-//               class="text-gray-300"
-//               stroke-width="5"
-//               stroke="currentColor"
-//               fill="transparent"
-//               r="15.9155"
-//               cx="16"
-//               cy="16"
-//             />
-//             {/* Percentage value */}
-//             <circle
-//               class="text-blue-600"
-//               stroke-width="5"
-//               stroke-dasharray="100 100"
-//               stroke-dashoffset={strokeDashOffsetValue}
-//               stroke-linecap="round"
-//               stroke="currentColor"
-//               fill="transparent"
-//               transition="stroke-dashoffset 1s ease-in-out"
-//               r="15.9155"
-//               cx="16"
-//               cy="16"
-//             />
-//           </svg>
-//           <span class="absolute text-xl text-blue-700">{strength}</span>
-//         </div>
-//         <label className="ml-2 mr-2 font-bold self-center">{title}</label>
-//         <div className="w-full self-center">{value}</div>
-//       </div>
-//     </div>
-//   )
-// }
--- components/post-header.js
+++ components/post-header.js
@@ -1,47 +1,18 @@
-// import Avatar from '../components/avatar'
+
 import Date from '../components/date'
-// import CoverImage from '../components/cover-image'
 import PostTitle from '../components/post-title'
-export default function PostHeader({ title, coverImage, updatedAt, shortDescription, type, tags, tokenStrength }) {
+import { CircularProgressbar } from 'react-circular-progressbar';
 
-  // https://codepen.io/nkunic/pen/jXBZWV
-  const percentageComplete = tokenStrength / 10
-  const strokeDashOffsetValue = 100 - (percentageComplete * 100)
+export default function PostHeader({ title, updatedAt, shortDescription, type, tags, tokenStrength }) {
 
+  const percentageComplete = tokenStrength * 10
 
   return (
     <>
       <PostTitle>{title}</PostTitle>
-      <div        
-        class="inline-flex items-center justify-center overflow-hidden rounded-full bottom-5 left-5"
-      >
-        <svg class="w-20 h-20" viewBox="-1 -1 34 34">
-          {/* Background */}
-          <circle
-            class="text-gray-300"
-            stroke-width="5"
-            stroke="currentColor"
-            fill="transparent"
-            r="15.9155"
-            cx="16"
-            cy="16"
-          />
-          {/* Percentage value */}
-          <circle
-            class="text-blue-600"
-            stroke-width="5"
-            stroke-dasharray="100 100"
-            stroke-dashoffset={strokeDashOffsetValue}
-            stroke-linecap="round"
-            stroke="currentColor"
-            fill="transparent"
-            transition= "stroke-dashoffset 1s ease-in-out"
-            r="15.9155"
-            cx="16"
-            cy="16"
-          />
-        </svg>
-        <span class="absolute text-xl text-blue-700">{tokenStrength}</span>
+
+      <div className='w-16 h-16'>
+        <CircularProgressbar value={percentageComplete} text={`${percentageComplete}`} />
       </div>
       <div className="mb-6 text-lg">
         <h1>{shortDescription}</h1>
