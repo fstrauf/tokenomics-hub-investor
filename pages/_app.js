66e90de95ea8fcba687b2b535a67fa6e45f106d5
fstrauf <f.strauf@gmail.com>
2022-11-04 20:20:21 +1100

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/index.css'
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <>
+    <GoogleAnalytics trackPageViews />
+    <Component {...pageProps} />
+    </>
+  ) 
 }
 
 export default MyApp
