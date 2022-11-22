b09293d98a4ed399d1005ff48c2035391a0d304c
fstrauf <f.strauf@gmail.com>
2022-11-22 18:34:43 +1100

    token strength styling

--- components/breakdown-box.js
+++ components/breakdown-box.js
@@ -7,14 +7,11 @@ export default function BreakdownBox({ value, strength, title }) {
   return (
     <div className="m-2">
       <div class="flex flex-row">
-        {/* <div className="grid grid-cols-1"> */}
         <div className='w-10 h-10 m-auto'>
-            <CircularProgressbar value={percentageComplete} text={`${percentageComplete}`} />
-          </div>
-        <div className="">
+          <CircularProgressbar value={percentageComplete} text={`${percentageComplete}`} />
+        </div>
+        <div className="m-auto w-40">
           <h1 className="ml-2 mr-2 font-bold">{title}</h1>
-          {/* <div class="flex"> */}
-          {/* <div class="justify-center m-2 inline-flex items-center overflow-hidden rounded-full bottom-5 left-5"> */}
         </div>
         <div className="w-full self-center">{value}</div>
       </div>
