ab2516400e5bb0848213f1eecc3cda9aa0f7ea40
fstrauf <f.strauf@gmail.com>
2023-02-08 16:32:25 +1100

    update script loading

--- pages/thub.js
+++ pages/thub.js
@@ -3,6 +3,7 @@ import Layout from '../components/layout'
 // import Intro from '../components/intro'
 import Image from 'next/image'
 import Link from 'next/link'
+import Script from 'next/script'
 
 export default function tokenomicshub() {
   return (
@@ -44,14 +45,49 @@ export default function tokenomicshub() {
               free here.
             </p>
             <form
-              className="mt-5"
               method="post"
-              action="https://sendfox.com/form/m89qv6/mn87gq"
-              className="sendfox-form"
-              id="mn87gq"
+              action="https://sendfox.com/form/m89qv6/m748dd"
+              class="sendfox-form"
+              id="m748dd"
               data-async="true"
               data-recaptcha="true"
             >
+              <p>
+                {/* <label for="sendfox_form_email">Email: </label> */}
+                <input
+                  type="email"
+                  id="sendfox_form_email"
+                  placeholder="Email"
+                  name="email"
+                  className="mt-2 mb-2 block w-60 rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 "
+                  required
+                />
+              </p>
+              {/* <!-- no botz please --> */}
+              <div
+                // style="position: absolute; left: -5000px;"
+                aria-hidden="true"
+              >
+                <input
+                  type="text"
+                  name="a_password"
+                  tabindex="-1"
+                  value=""
+                  autocomplete="off"
+                />
+              </div>
+              <p>
+                <button
+                  className="mt-5 rounded-md bg-dao-red px-6 py-4 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+                  type="submit"
+                >
+                  Get Access Now!
+                </button>
+              </p>
+            </form>
+            <script src="https://sendfox.com/js/form.js"></script>
+
+            {/* <form method="post" action="https://sendfox.com/form/m89qv6/mn87gq" class="sendfox-form" id="mn87gq" data-async="true" data-recaptcha="true">
               <p className="mt-5">
                 <label htmlFor="sendfox_form_email">Email: </label>
                 <input
@@ -69,7 +105,6 @@ export default function tokenomicshub() {
                   agree to receive email updates and promotions.
                 </label>
               </div>
-              {/* <!-- no botz please --> */}
               <div className="" aria-hidden="true">
                 <input
                   type="text"
@@ -88,7 +123,7 @@ export default function tokenomicshub() {
                 </button>
               </div>
             </form>
-            <script src="https://sendfox.com/js/form.js"></script>
+            <script src="https://sendfox.com/js/form.js"></script> */}
           </div>
           <hr className="my-4 mx-auto h-1 w-48 rounded border-0 bg-gray-100 dark:bg-gray-700 md:my-10"></hr>
           <div className="mb-5">
@@ -98,10 +133,11 @@ export default function tokenomicshub() {
                 <p>(see what's included)</p>
               </a>
             </div>
-            <script
+            <Script src="https://js.stripe.com/v3/pricing-table.js" async/>
+            {/* <script
               async
               src="https://js.stripe.com/v3/pricing-table.js"
-            ></script>
+            ></script> */}
             <stripe-pricing-table
               pricing-table-id="prctbl_1LmR42Ks0xSuCAmfiO54IWu1"
               publishable-key="pk_live_51LUqnsKs0xSuCAmfmBBT45ICtEtzj8EkcJK7e2SFncAnbVzjEO4WFY9X3C8Ih1OlevrEAXZmiu86jszeLfQDJ5Xv000IKYBg6T"
