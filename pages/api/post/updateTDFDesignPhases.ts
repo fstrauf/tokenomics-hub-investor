ac0df1a901e0924938af3b4e9d100b96cd0750a2
fstrauf <f.strauf@gmail.com>
2023-02-17 21:04:39 +1100

    fixed no parent

--- pages/api/post/updateTDFDesignPhases.ts
+++ pages/api/post/updateTDFDesignPhases.ts
@@ -8,22 +8,26 @@ export default async function handle(req, res) {
     Resources = JSON.stringify(values.Resources)
   }
 
+  const parentPhase = values.parentPhaseId
+    ? {
+        parentPhase: {connect: { phaseId: parseInt(values.parentPhaseId) }},
+      }
+    : {}
+
   const response = await prisma.designPhases.upsert({
     where: {
       phaseId: parseInt(values.phaseId),
     },
     update: {
-      // id: parseInt(values.id),
       name: values.name,
       phaseId: parseInt(values.phaseId),
-      parentPhase: { connect: { phaseId: parseInt(values.parentPhaseId) } },
+      ...parentPhase,
       Resources: Resources,
     },
     create: {
-      // id: parseInt(values.id),
       phaseId: parseInt(values.phaseId),
       name: values.name,
-      parentPhase: { connect: { phaseId: parseInt(values.parentPhaseId) } },
+      ...parentPhase,
       Resources: Resources,
     },
   })
