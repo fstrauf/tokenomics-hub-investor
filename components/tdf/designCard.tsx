9207cce45a945a615bd28b683e23981fd0853b62
fstrauf <f.strauf@gmail.com>
2023-03-24 21:00:14 +1100

    mydesigns screen upgrade

--- components/tdf/designCard.tsx
+++ components/tdf/designCard.tsx
@@ -1,218 +1,55 @@
-import Router from 'next/router'
-import { useUser } from '@clerk/clerk-react/dist/hooks/useUser'
-import toast, { Toaster } from 'react-hot-toast'
-import { Menu, Transition, Dialog } from '@headlessui/react'
-import { Fragment, useState } from 'react'
-import { confirmAlert } from 'react-confirm-alert' // Import
-import 'react-confirm-alert/src/react-confirm-alert.css'
-import {
-  mandatoryFormValidate,
-  notifyStatusUpdate,
-  postStatus,
-} from '../../lib/helper'
-import { WEBSITE_URL_BASE } from '../../lib/constants'
-import GreaterThanArrow from '../../public/svg/greaterThanArrow'
-import AddUserIcon from '../../public/svg/addUserIcon'
-import SettingIcon from '../../public/svg/settingsIcon'
-import Modal from 'react-modal'
+import Link from 'next/link'
 
-const customStyles = {
-  content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-  },
-}
-
-export default function DesignCard({ posts, context }) {
-  const { user } = useUser()
-  const [isSubmitting, setSubmitting] = useState(false)
-  const contributor = user?.publicMetadata?.contributor || false
-
-  const publishPost = async (post) => {
-    setSubmitting(true)
-    await fetch(`/api/post/publish/${post.id}`, {
-      method: 'PUT',
-    })
-    notifyStatusUpdate(
-      post.authorEmail,
-      postStatus.published,
-      `${WEBSITE_URL_BASE}/posts/${post.slug}`
-    )
-    setSubmitting(false)
-    await Router.push('/')
-  }
-
-  const deleteDraft = async (id: String) => {
-    setSubmitting(true)
-    await fetch(`/api/post/delete/${id}`, {
-      method: 'PUT',
-    })
-    setSubmitting(false)
-    await Router.push(`/${context}`)
-  }
-
-  const confirmDelete = async (id: String) => {
-    confirmAlert({
-      message: 'Are you sure to delete this draft?.',
-      buttons: [
-        {
-          label: 'Yes',
-          onClick: () => deleteDraft(id),
-        },
-        {
-          label: 'No',
-        },
-      ],
-    })
-  }
-
-  async function sendToReview(
-    // event: MouseEvent<HTMLButtonElement, MouseEvent>,
-    post,
-    close
-  ): void {
-    const errors = mandatoryFormValidate(post)
-
-    if (Object.keys(errors).length > 0) {
-      toast.error('Some required fields are missing!', {
-        position: 'bottom-right',
-      })
-    } else {
-      setSubmitting(true)
-      const postId = post.id
-      const body = { status: postStatus.reviewRequired, postId }
-
-      const response = await fetch('/api/post/updateStatus', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(body),
-      })
-
-      if (!response.ok) {
-        const error = await response.text()
-        toast.error(JSON.parse(error).error, { position: 'bottom-right' })
-        throw new Error(error)
-      } else {
-        toast.success('Sent to review', { position: 'bottom-right' })
-        notifyStatusUpdate(
-          post.authorEmail,
-          postStatus.reviewRequired,
-          `${WEBSITE_URL_BASE}/editPost/${postId}`
-        )
-      }
-      setSubmitting(false)
-      close()
-      await Router.replace(Router.asPath)
-    }
-  }
-
-  async function reviewComplete(
-    event: MouseEvent<HTMLButtonElement, MouseEvent>,
-    post,
-    close
-  ): void {
-    const postId = post.id
-    const body = { status: postStatus.reviewComplete, postId }
-    setSubmitting(true)
-
-    const response = await fetch('/api/post/updateStatus', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(body),
-    })
-
-    if (!response.ok) {
-      const error = await response.text()
-      toast.error(JSON.parse(error).error, { position: 'bottom-right' })
-      throw new Error(error)
-    } else {
-      toast.success('Review completed', { position: 'bottom-right' })
-      notifyStatusUpdate(
-        post.authorEmail,
-        postStatus.reviewComplete,
-        `${WEBSITE_URL_BASE}/editPost/${postId}`
-      )
-    }
-    setSubmitting(false)
-    close()
-    await Router.replace(Router.asPath)
-  }
-
-  const clickOnGreaterThanArrow = () => {
-    console.log('clicked on greater arrow')
-  }
-
-  const clickOnAddUser = () => {
-    console.log('clicked on add user')
-  }
-  const clickOnSettings = () => {
-    console.log('clicked on settings')
-  }
-
-  const [modalIsOpen, setIsOpen] = useState(false)
-
-  function openModal() {
-    setIsOpen(true)
-  }
-
-  function afterOpenModal() {
-    // references are now sync'd and can be accessed.
-  }
-
-  function closeModal() {
-    setIsOpen(false)
-  }
-
-  return (
-    <div className="static overflow-x-auto rounded-[10px] bg-white px-8 py-6">
-      <Toaster />
-      <div className="mb-2 grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3  xl:grid-cols-4">
-        {posts?.map((post, index) => {
-          return (
-            <div key={index} className="h-120  rounded-[10px] border">
-              <div className="grid h-[90px] grid-cols-2">
-                <div className="ml-4 mt-2 text-lg font-bold">{post.title}</div>
-                <div className="mt-[11px] mr-2 flex  justify-end text-lg font-bold">
-                  <span
-                    className="cursor-pointer"
-                    onClick={clickOnGreaterThanArrow}
-                  >
-                    {<GreaterThanArrow />}
-                  </span>
-                </div>
-              </div>
-              <div className="">
-                <div className="grid h-[30px] grid-cols-2 rounded-b-[10px] border-t">
-                  <div className="flex cursor-pointer justify-center border-r">
-                    <span className="mt-1" onClick={openModal}>
-                      {<AddUserIcon />}
-                    </span>
-                    <Modal
-                      isOpen={modalIsOpen}
-                      onRequestClose={closeModal}
-                      style={customStyles}
-                      contentLabel="Example Modal"
-                    >
-                      <button onClick={closeModal}>close</button>
-                      <div>Add User Screen..</div>
-                    </Modal>
-                  </div>
-
-                  <div className="flex cursor-pointer justify-center">
-                    <span className="mt-1" onClick={clickOnSettings}>
-                      {<SettingIcon />}
-                    </span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          )
-        })}
+export default function DesignCard({ post, context }) {
+  console.log("🚀 ~ file: designCard.tsx:4 ~ DesignCard ~ post:", post)
+  return (    
+      <div className="m-5 w-80 h-64 max-w-sm rounded-lg border border-gray-200 bg-white shadow-md">
+        <div className="flex justify-end px-4 pt-4"></div>
+        <div className="flex flex-col items-center pb-10">
+          {/* <img
+            className="mb-3 h-24 w-24 rounded-full shadow-lg"
+            src={post?.title}
+          /> */}
+          {/* <h5 className="mb-1 text-xl font-medium text-gray-900">
+            {post?.username ?? post?.firstName}
+          </h5> */}
+          <span className="text-sm text-gray-500">
+            {post.title}
+          </span>
+          <div className="flex w-full items-center justify-center divide-x divide-solid divide-gray-400 py-4">
+            <span className="px-2 text-center">
+              <span className="font-bold text-gray-700">Author: </span>
+              <span className="text-gray-600">{post.author}</span>
+            </span>
+            <span className="px-2 text-center">
+              {/* <span className="font-bold text-gray-700">
+                {post?.articleCount}
+              </span> */}
+              {/* <span className="text-gray-600"> protocols listed</span> */}
+            </span>
+          </div>
+          <div className="flex flex-wrap justify-center">
+            {post?.cat?.map((c) => {
+              return (
+                <span
+                  key={c.cat}
+                  className="mr-2 mb-2 rounded-full bg-gray-200 px-3 py-1 text-sm font-bold text-gray-700 shadow-sm"
+                >
+                  {c.cat} ({Number(c.count)})
+                </span>
+              )
+            })}
+          </div>
+          <div className="mt-4 flex space-x-3 md:mt-6">
+            <Link
+              as={`/posts/${post?.id}`}
+              href="/posts/[slug]"
+              className="inline-flex items-center rounded-lg bg-dao-red px-4 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300"
+            >
+              Edit
+            </Link>
+          </div>
+        </div>
       </div>
-    </div>
   )
 }
