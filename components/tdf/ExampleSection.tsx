ec6263bf2310d049dded598ed38e18f0490978b9
Kanti Lal <kantilal@yudiz.com>
2023-03-28 16:35:31 +0530

    resource and example sections are now collapsible.

--- components/tdf/ExampleSection.tsx
+++ components/tdf/ExampleSection.tsx
@@ -1,7 +1,14 @@
+import { Disclosure } from '@headlessui/react'
 import Image from 'next/image'
 import { useState } from 'react'
+import ChevronIcon from '../../public/svg/chevron'
+import FormDivider from '../form/FormDivider'
 
-export default function ExampleSection({ content, exampleField, exampleDetail }) {
+export default function ExampleSection({
+  content,
+  exampleField,
+  exampleDetail,
+}) {
   const [example, setExample] = useState({})
   const [isSubelementClicked, setIsSubelementClicked] = useState(false)
 
@@ -10,7 +17,7 @@ export default function ExampleSection({ content, exampleField, exampleDetail })
     setIsSubelementClicked(true)
   }
 
-  let ExampleDetail = ({onGoBack, example, exampleField}) => {
+  let ExampleDetail = ({ onGoBack, example, exampleField }) => {
     return (
       <div className="flex flex-col justify-between">
         <div>
@@ -31,17 +38,17 @@ export default function ExampleSection({ content, exampleField, exampleDetail })
       </div>
     )
   }
-  
-  if(exampleDetail !== undefined){
+
+  if (exampleDetail !== undefined) {
     ExampleDetail = exampleDetail
-  } 
+  }
 
   function ExamplesSelector() {
     return (
       <div>
         <div className="flex gap-6 overflow-x-auto">
           {content?.map((c) => (
-            <div className="h-52 flex flex-col justify-between">
+            <div className="flex h-52 flex-col justify-between">
               <div className="m-auto w-9 sm:w-16">
                 <div className="relative m-auto h-24 rounded-lg">
                   <Image
@@ -61,7 +68,6 @@ export default function ExampleSection({ content, exampleField, exampleDetail })
                   Details
                 </button>
               </div>
-
             </div>
           ))}
         </div>
@@ -69,19 +75,35 @@ export default function ExampleSection({ content, exampleField, exampleDetail })
     )
   }
 
-
-
   return (
-    <div className="">
-      <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 ">
+    <Disclosure>
+      {({ open }) => (
+        <>
+          <div className="mt-2 rounded-lg border-4  border-gray-300 border-opacity-20">
+            <Disclosure.Button className="flex w-full justify-between rounded-sm bg-gray-300 bg-opacity-20 px-4 py-2 text-left text-sm font-medium hover:bg-opacity-100 focus:outline-none focus-visible:ring focus-visible:ring-gray-500 focus-visible:ring-opacity-75">
+              <FormDivider text="Example" />
+              <ChevronIcon />
+            </Disclosure.Button>
+            <Disclosure.Panel className="px-2 pb-2 text-sm text-gray-500">
+              <div className="">
+                {/* <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 ">
         Examples
-      </h5>
+      </h5> */}
 
-      {isSubelementClicked ? (
-        <ExampleDetail onGoBack={() => setIsSubelementClicked(false)} example={example} exampleField={exampleField} />
-      ) : (
-        <ExamplesSelector />
+                {isSubelementClicked ? (
+                  <ExampleDetail
+                    onGoBack={() => setIsSubelementClicked(false)}
+                    example={example}
+                    exampleField={exampleField}
+                  />
+                ) : (
+                  <ExamplesSelector />
+                )}
+              </div>
+            </Disclosure.Panel>
+          </div>
+        </>
       )}
-    </div>
+    </Disclosure>
   )
 }
