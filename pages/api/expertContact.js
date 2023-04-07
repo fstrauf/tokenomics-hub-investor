c396c424fec69944718a7459c881cf527a13a13a
Kanti Lal <kantilal@yudiz.com>
2023-04-07 16:11:53 +0530

    Book an expert form sends message twice is resolved

--- pages/api/expertContact.js
+++ pages/api/expertContact.js
@@ -1,32 +1,21 @@
 export default async function handler(req, res) {
   const { values } = req.body
-  let msg = ''
-  msg =
-    '<@&1008672386079522836> \n\nA protocol has recently requested feedback from the Tokenomics DAO community. Any contributor can take part and share their opinion on:\n\n-The protocol design \n-The tokenomics design \n**Be as concise and as critical as possible**' +
-    '\n\nEmail: ' +
-    values.email +
-    '\nLink: ' +
-    values.url +
-    '\nMessage: ' +
-    values.message
 
   fetch(process.env.DISCORD_CONSULTING, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
-      content: msg,
-      // 'Name: ' +
-      // values.name +
-      // '\nEmail: ' +
-      // values.email +
-      // '\nTimeline: ' +
-      // values.timeline +
-      // '\nURL: ' +
-      // values.url +
-      // '\nMessage: ' +
-      // values.message,
+      content:
+        'Name: ' +
+        values.name +
+        '\nEmail: ' +
+        values.email +
+        '\nTimeline: ' +
+        values.timeline +
+        '\nMessage: ' +
+        values.message,
     }),
   })
-    .then((res) => console.log('res', res))
+    .then((res) => console.log(res))
     .catch((err) => console.error(err))
 }
