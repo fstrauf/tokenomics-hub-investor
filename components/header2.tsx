a3289ed53850c5e34a08019aa36c5d8481ef904b
fstrauf <f.strauf@gmail.com>
2023-04-24 15:56:28 +1000

    routing

--- components/header2.tsx
+++ components/header2.tsx
@@ -37,7 +37,7 @@ export default function Header2({ mode = headerStatus.main, children = null }) {
               title="Products & Services"
             >
               <HeaderGenericSection
-                pathName="/tdsIntro"
+                pathName="/tdsLandingPage"
                 title="Tokenomics Design Space"
               />
               <HeaderGenericSection
