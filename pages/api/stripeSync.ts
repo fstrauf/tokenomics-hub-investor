180d5ed3ad9aec1f292e30d1adfc97aae529d216
fstrauf <f.strauf@gmail.com>
2023-05-03 08:55:29 +1000

    basic stripe listener

--- pages/api/stripeSync.ts
+++ pages/api/stripeSync.ts
@@ -1,10 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import Stripe from 'stripe'
-import { buffer } from "micro";
+import { buffer } from 'micro'
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2022-11-15',
-})
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+}
 
 type Data = {
   name: string
@@ -14,32 +16,41 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  console.log("🚀 ~ file: stripeSync.ts:16 ~ req:", req)
-  const sig = req.headers['stripe-signature']
-  const buf = await buffer(req);
-  let event
-  try {
-    event = await stripe.webhooks.constructEvent(
-      buf,
-      sig,
-      process.env.STRIPE_WEBHOOK_SECRET
-    )
-    switch (event?.type) {
-      case 'checkout.session.completed':
-        const paymentIntentSucceeded = event.data.object
-        console.log(
-          '🚀 ~ file: stripeSync.ts:35 ~ paymentIntentSucceeded:',
-          paymentIntentSucceeded
-        )
-        // Then define and call a function to handle the event payment_intent.succeeded
-        break
-      // ... handle other event types
-      default:
-        console.log(`Unhandled event type ${event?.type}`)
+  // const stripe = initStripe(process.env.STRIPE_SECRET_KEY);
+  if (req.method === 'POST') {
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+      apiVersion: '2022-11-15',
+    })
+    const sig = req.headers['stripe-signature']
+    const buf = await buffer(req)
+    let event
+    try {
+      event = await stripe.webhooks.constructEvent(
+        buf,
+        sig,
+        process.env.STRIPE_WEBHOOK_SECRET
+      )
+      console.log("🚀 ~ file: stripeSync.ts:34 ~ event?.type:", event?.type)
+      switch (event?.type) {        
+        case 'checkout.session.completed':
+          const paymentIntentSucceeded = event.data.object
+          console.log(
+            '🚀 ~ file: stripeSync.ts:35 ~ paymentIntentSucceeded:',
+            paymentIntentSucceeded
+          )
+          // Then define and call a function to handle the event payment_intent.succeeded
+          break
+        // ... handle other event types
+        default:
+          console.log(`Unhandled event type ${event?.type}`)
+      }
+      res.status(200).json({ received: true })
+    } catch (err) {
+      console.error(`Error verifying Stripe webhook: ${err.message}`)
+      res.status(400).json({ error: `Webhook Error: ${err.message}` })
     }
-    res.status(200).json({ received: true })
-  } catch (err) {
-    console.error(`Error verifying Stripe webhook: ${err.message}`)
-    res.status(400).json({ error: `Webhook Error: ${err.message}` })
+  } else {
+    res.setHeader('Allow', 'POST')
+    res.status(405).end('Method Not Allowed')
   }
 }
--- pages/manage-subscriptions.tsx
+++ pages/manage-subscriptions.tsx
@@ -4,7 +4,6 @@ import Script from 'next/script'
 import { useUser } from '@clerk/clerk-react/dist/hooks/useUser'
 
 export default function ManageSubscriptions() {
-
   const { user } = useUser()
   return (
     <>
@@ -13,7 +12,7 @@ export default function ManageSubscriptions() {
           <div className="mt-10">
             <Script src="https://js.stripe.com/v3/pricing-table.js" async />
             <stripe-pricing-table
-              pricing-table-id="prctbl_1N2jx5Ks0xSuCAmfbSPvhMPY"
+              pricing-table-id="prctbl_1N38wkKs0xSuCAmfu87BjsFs"
               publishable-key="pk_live_51LUqnsKs0xSuCAmfmBBT45ICtEtzj8EkcJK7e2SFncAnbVzjEO4WFY9X3C8Ih1OlevrEAXZmiu86jszeLfQDJ5Xv000IKYBg6T"
               client-reference-id={user?.id}
             ></stripe-pricing-table>
