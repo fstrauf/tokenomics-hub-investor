31794b9d62918b028eb6967fbc8b00beec1ca33a
fstrauf <f.strauf@gmail.com>
2023-05-05 09:16:26 +1000

    subscription page upgrade

--- pages/manage-subscriptions.tsx
+++ pages/manage-subscriptions.tsx
@@ -1,17 +1,24 @@
 import Layout from '../components/layout'
 import { headerStatus } from '../lib/helper'
 // import Script from 'next/script'
-import { useUser } from '@clerk/clerk-react/dist/hooks/useUser'
+// import { useUser } from '@clerk/clerk-react/dist/hooks/useUser'
 import SubscriptionTable from './SubscriptionTable'
+import SubscriptionStatus from './SubscriptionStatus'
+import { getAuth } from '@clerk/nextjs/dist/server/getAuth'
+import { AuthData } from '@clerk/nextjs/dist/server/types'
+import prisma from '../lib/prisma'
+import { GetServerSideProps } from 'next'
 
-export default function ManageSubscriptions() {
-  const { user } = useUser()
+
+export default function ManageSubscriptions(props) {
+  // const { user } = useUser()
   return (
     <>
       <Layout mode={headerStatus.main}>
         <>
-          <SubscriptionTable />
 
+          <SubscriptionTable />
+          <SubscriptionStatus customerId={props?.subscription?.stripeCustomerId} />
           <div>
             <a
               href="https://billing.stripe.com/p/login/8wMcQ7b9w9Yx95S000"
@@ -26,4 +33,15 @@ export default function ManageSubscriptions() {
       </Layout>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { userId }: AuthData = getAuth(context.req)
+  const subscription = await prisma.subscriptions.findUnique({where:{authorClerkId: userId}})
+
+  return {
+    props: {      
+      Subscription: subscription || null,
+    },
+  }
 }
