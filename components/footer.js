f0f546f8db9bb92afcd77a033a55586f542896d7
fstrauf <f.strauf@gmail.com>
2023-05-10 09:14:53 +1000

    list a token to footer

--- components/footer.js
+++ components/footer.js
@@ -40,6 +40,9 @@ export default function Footer() {
             <Link href="/privacy-policy" className="hover:underline">
               Privacy Policy
             </Link>
+            <Link href="/newPost" className="hover:underline">
+              List a token
+            </Link>
           </div>
           </div>
           <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
