9435ae9fa431509ccde03baaa8a0a575ae72df96
fstrauf <f.strauf@gmail.com>
2023-05-31 06:51:42 +0200

    tdf coming soon dialog

---
+++ components/tdf/TDFComingSoon.tsx
@@ -0,0 +1,13 @@
+
+export default function TDFComingSoon({ 
+}) {
+  
+  return (
+    <div className="flex flex-col">
+      <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 ">
+        Coming soon
+      </h5>
+      <p className='prose'>Check back shortly, while we work on this new section!</p>
+    </div>
+  )
+}
