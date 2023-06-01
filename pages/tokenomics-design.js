068321c083fd21ff6a850ab157927a0d9717e821
fstrauf <f.strauf@gmail.com>
2023-06-01 07:39:31 +0200

    cleanup landing page

--- pages/tokenomics-design.js
+++ pages/tokenomics-design.js
@@ -1,22 +1,12 @@
-// import { getAllPostsForHome } from '../lib/api'
-// import Layout from '../components/layout'
-// import Intro from '../components/intro'
 import Image from 'next/image'
 import Link from 'next/link'
 import Header2 from '../components/header2'
 import ThubLogo from '../public/svg/thub-logo'
-// import Script from 'next/script'
 import LiteYouTubeEmbed from 'react-lite-youtube-embed'
 import 'react-lite-youtube-embed/dist/LiteYouTubeEmbed.css'
 
 export default function TokenomicsDesignSpace() {
-  // const videoLink = (
-  //   <a href="https://youtu.be/dq_SowJgRvE" target="_blank">
-  //     <button className="mt-5 rounded-md border-2 border-dao-red bg-white px-6 py-4 text-sm font-medium text-dao-red hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
-  //       Watch Quick Demo
-  //     </button>
-  //   </a>
-  // )
+
   const designLink = (
     <Link href="/newDesign">
       <button className="mt-5 rounded-md bg-dao-red px-6 py-4 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
